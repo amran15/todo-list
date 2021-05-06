b0fc8e5d8841ef01a713049df0aed98e6e8679b3
Amran Ahmed <amranahmedd@gmail.com>
2021-05-06 14:26:13 -0500

    updated styling for UI, rewrote logic for app.js

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 // grab object
-let input = document.querySelector('#add');
-let btn = document.querySelector('#btn');
-let list = document.querySelector('#list');
-let element = document.getElementsByTagName('li');
+let input = document.querySelector('.add');
+let btn = document.querySelector('.btn');
+let list = document.querySelector('.list');
+// let element = document.getElementsByTagName('li');
 
-//function -- add element to our btn click 
-btn.addEventListener('click', () => {
-    let text = input.value;
-    if(text === ' '){
-        alert('you must write something');
-    }else{
-        let li = document.createElement('li');
-        li.innerHTML = text;
-        list.insertBefore(li, list.childNodes[0]);
-        input.value = '';
-    }
-})
-//function -- mark completed task
-list.addEventListener('click', e => {
-if(e.target.tagName === 'LI'){
-    e.target.classList.toggle('checked')
+//EventListener
+btn.addEventListener('click', addTodo);
+
+//Functions
+
+function addTodo(event) {
+    //prevents form from submitting
+    event.preventDefault(); 
+    //create div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo')
+    //create Li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'make coffee';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
 }
-})
-
-// delete function 
